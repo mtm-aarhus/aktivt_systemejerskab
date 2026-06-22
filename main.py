c22670aa6da4284e8f87a65799c709132b0bdbaa
Oguz Kilic <kiog@aarhus.dk>
2026-06-22 09:28:28 +0200

    fix: brug --prefer-binary ved pip install for at undgaa wheel-build fejl

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ script_directory = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_directory)
 
 subprocess.run("python -m venv .venv", check=True)
-subprocess.run(r'.venv\Scripts\pip install .', check=True)
+subprocess.run(r'.venv\Scripts\pip install --prefer-binary .', check=True)
 
 command_args = [r".venv\Scripts\python", "-m", "robot_framework"] + sys.argv[1:]
 
